599c91e8978fa1ca5832ddc9230dd63a625a9a53
SUBIN <53263265+SUB1NKR@users.noreply.github.com>
2026-06-11 19:38:19 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+const loadingPage = document.querySelector("#loading");
+const slides = document.querySelectorAll(".safety-slide");
 
+const slideInterval = 3000;
+const totalLoadingTime = slides.length * slideInterval;
+
+
+let currentIndex = 0;
+
+const slideTimer = setInterval(() => {
+  slides[currentIndex].classList.remove("active");
+
+  currentIndex += 1;
+
+  if (currentIndex >= slides.length) {
+    clearInterval(slideTimer);
+    return;
+  }
+
+  slides[currentIndex].classList.add("active");
+}, slideInterval);
+
+setTimeout(() => {
+  loadingPage.classList.add("is-hidden");
+
+  setTimeout(() => {
+    loadingPage.style.display = "none";
+  }, 800);
+}, totalLoadingTime);
